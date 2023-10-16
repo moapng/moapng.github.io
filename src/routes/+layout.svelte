4a8f40541f322bf03fca7dd4e30c5090d9002771
moa <larssonmoaml@gmail.com>
2023-10-16 22:19:59 +0200

    added fonts, bg, basic styling, sass

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,31 @@
-<script lang="ts"></script>
+<script lang="ts">
+	import Box from './Box.svelte';
 
-<slot />
+	import '$lib/styles/common.scss';
+</script>
+
+<div class="app">
+	<div class="header">
+		<h1>Moa</h1>
+	</div>
+	<Box>
+		<slot />
+	</Box>
+</div>
+
+<style lang="scss">
+	@import '$lib/styles/variables.scss';
+
+	.app {
+		margin: auto;
+		text-align: center;
+		flex-shrink: 0;
+	}
+	.header {
+		border-radius: 0rem 0rem 4.375rem 4.375rem;
+		background: $lavender-dark;
+		margin: auto;
+		text-align: center;
+		width: 40vw;
+	}
+</style>
