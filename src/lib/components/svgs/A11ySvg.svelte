7635e927cdaab6a8648b8f4fea04526072f2f3e4
moa <larssonmoaml@gmail.com>
2023-10-21 00:41:22 +0200

    more icons, styling

---
+++ src/lib/components/svgs/A11ySvg.svelte
@@ -0,0 +1,32 @@
+<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<rect x="0.5" y="0.5" width="31" height="31" rx="7.5" fill="#A1E6C0" />
+	<path
+		d="M4 19.7641L4.79512 17.5445M4.79512 17.5445L6.30845 13.0044L7.82182 17.5445M4.79512 17.5445H7.82182M8.62895 19.7641L7.82182 17.5445"
+		stroke="#25563B"
+		stroke-width="1.5"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+	<path
+		d="M25.5786 16.132L28 12.5M25.5786 16.132L23.1572 12.5M25.5786 16.132V19.7641"
+		stroke="#25563B"
+		stroke-width="1.5"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+	<path
+		d="M11.0503 14.9214L13.4717 12.5V19.7641"
+		stroke="#25563B"
+		stroke-width="1.5"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+	<path
+		d="M17.1035 14.9214L19.5249 12.5V19.7641"
+		stroke="#25563B"
+		stroke-width="1.5"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+	<rect x="0.5" y="0.5" width="31" height="31" rx="7.5" stroke="#25563B" />
+</svg>
--- src/routes/kompetenser/+page.svelte
+++ src/routes/kompetenser/+page.svelte
@@ -9,36 +9,37 @@
 	import NodeSvg from '$lib/components/svgs/NodeSvg.svelte';
 	import CSvg from '$lib/components/svgs/CSvg.svelte';
 	import SqlSvg from '$lib/components/svgs/SqlSvg.svelte';
+	import ScrumSvg from '$lib/components/svgs/ScrumSvg.svelte';
+	import A11ySvg from '$lib/components/svgs/A11ySvg.svelte';
+	import PostmanSvg from '$lib/components/svgs/PostmanSvg.svelte';
 </script>
 
 <div class="content">
-	<div class="text">
-		<h1>Mina kompetenser</h1>
-		<div class="grid">
-			<ul>
-				<li>
-					<SvelteSvg /> SvelteKit
-				</li>
-				<li><ReactSvg />React</li>
-				<li><TsSvg /> TypeScript</li>
-				<li><JsSvg />JavaScript</li>
-			</ul>
-			<ul>
-				<li><HtmlSvg />HTML5</li>
-				<li><CssSvg />CSS3</li>
-				<li><NpmSvg />npm</li>
-				<li><NodeSvg />nodeJS</li>
-			</ul>
-			<ul>
-				<li><CSvg /> C#</li>
-				<li><SqlSvg />SQL</li>
-				<li>Postman</li>
-			</ul>
-			<ul>
-				<li>WCAG</li>
-				<li>Scrum</li>
-			</ul>
-		</div>
+	<h1>Mina kompetenser</h1>
+	<div class="grid">
+		<ul>
+			<li>
+				<SvelteSvg />SvelteKit
+			</li>
+			<li><ReactSvg />React</li>
+			<li><TsSvg /> TypeScript</li>
+			<li><JsSvg />JavaScript</li>
+		</ul>
+		<ul>
+			<li><HtmlSvg />HTML5</li>
+			<li><CssSvg />CSS3</li>
+			<li><NpmSvg />npm</li>
+			<li><NodeSvg />nodeJS</li>
+		</ul>
+		<ul>
+			<li><CSvg /> C#</li>
+			<li><SqlSvg />SQL</li>
+			<li><PostmanSvg />Postman</li>
+		</ul>
+		<ul>
+			<li><A11ySvg />WCAG</li>
+			<li><ScrumSvg /> Scrum</li>
+		</ul>
 	</div>
 </div>
 
@@ -46,31 +47,28 @@
 	@import '$lib/styles/variables.scss';
 	.content {
 		z-index: 2;
-
-		display: flex;
+		position: relative;
 	}
+
 	h1 {
 		color: $lavender;
 		margin-top: 2rem;
 	}
-	.text {
-		z-index: inherit;
 
-		width: 44rem;
-		margin: auto;
-	}
 	.grid {
-		//använd grid column ist?
 		display: grid;
 		grid-template-columns: auto auto auto auto;
-		column-gap: 2rem;
-		column-rule: 1rem double #ff00ff;
 	}
+
 	li {
 		display: flex;
+		align-items: center;
+		margin-bottom: 1.5rem;
+		gap: 1rem;
 	}
+
 	:global(svg) {
-		width: 2.5rem;
-		height: 2.5rem;
+		width: 3rem;
+		height: 3rem;
 	}
 </style>
