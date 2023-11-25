c2cf892431b73e7d3e344c1b151d6f0ce7fed612
moa <larssonmoaml@gmail.com>
2023-11-25 02:48:52 +0100

    npm run build bug fix

--- src/routes/projekt/[name]/+page.ts
+++ src/routes/projekt/[name]/+page.ts
@@ -1,12 +1,25 @@
 import type { IProject } from '$lib/Interfaces';
 import projects from '$lib/projectData.json';
-export const prerender = false;
+import type { EntryGenerator } from './$types';
+
+export const entries: EntryGenerator = () => {
+	let entries: any = [];
+	projects.forEach((project) => {
+		entries.push({ name: project.title });
+	});
+
+	return entries;
+};
+
+export const prerender = true;
 
 export const load = ({ params }) => {
 	console.log(params);
 	const { name } = params;
 
-	const project: IProject = projects.find((project: IProject) => project.title === name);
+	const project: IProject | undefined = projects.find(
+		(project: IProject) => project.title === name
+	);
 
 	if (!project) {
 		return { status: 404, error: new Error('Project not found') };
